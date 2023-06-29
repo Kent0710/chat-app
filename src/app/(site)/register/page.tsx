760763ee1434e7b6070631e1803ebb873502008d
Kent0710 <christiankent001@gmail.com>
2023-06-29 10:57:27 +0800

    no origin host function

--- src/app/(site)/register/page.tsx
+++ src/app/(site)/register/page.tsx
@@ -15,7 +15,7 @@ export default function RegisterPage() {
         e.preventDefault();
 
         try {
-            const response = await fetch (`${fetchOriginHost()}/api/register`, {
+            const response = await fetch (`/api/register`, {
                 method : "POST",
                 headers : {"Content-Type" : "application/json"},
                 body : JSON.stringify({email, name, userType})
