dec3331cac62ad1f1e79974756ffc1fa2e67ee57
Kent0710 <christiankent001@gmail.com>
2023-06-30 21:51:01 +0800

    returned pusher credentials

--- src/libs/pusher.ts
+++ src/libs/pusher.ts
@@ -2,15 +2,15 @@ import PusherServer from "pusher"
 import PusherClient from "pusher-js"
 
 export const pusherServer = new PusherServer({
-    appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID || "",
-    key: process.env.NEXT_PUBLIC_PUSHER_KEY || "",
-    secret: process.env.NEXT_PUBLIC_PUSHER_SECRET || "",
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "",
+    appId: "1620740",
+    key: "3bf15af436f1ac8f2d25",
+    secret: "fe7f1683b4d5cc4301e6",
+    cluster: "ap1",
     useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-    process.env.NEXT_PUBLIC_PUSHER_KEY || "", {
-        cluster : process.env.NEXT_PUBLIC_PUSHER_CLUSTER || ""
+    "3bf15af436f1ac8f2d25", {
+        cluster : "ap1"
     }
 )
